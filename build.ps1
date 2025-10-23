0a07ffffc54f50f0cb4e14332ed4b15fcf66e517
奇葩の灵梦 <cutereimu@vip.qq.com>
2025-10-23 23:36:24 +0800

    修复编译脚本问题

--- build.ps1
+++ build.ps1
@@ -1,6 +1,4 @@
 Invoke-WebRequest -Uri 'https://github.com/AlexKnauth/silksong-autosplit-wasm/releases/latest/download/silksong_autosplit_wasm_stable.wasm' -OutFile 'silksong_autosplit_wasm_stable.wasm' -v
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LiveSplit/LiveSplit.AutoSplitters/master/LiveSplit.AutoSplitters.xml' -OutFile 'LiveSplit.AutoSplitters.xml' -v
 go build -ldflags "-s -w -H=windowsgui" -o sssplitmaker.exe github.com/CuteReimu/sssplitmaker
-go env -w GOOS=darwin
-go env -w GOARCH=arm64
-go build -ldflags "-s -w" -o sssplitmaker github.com/CuteReimu/sssplitmaker
+cmd.exe /c 'set GOOS=darwin&& set GOARCH=arm64&& go build -ldflags "-s -w" -o sssplitmaker github.com/CuteReimu/sssplitmaker'
