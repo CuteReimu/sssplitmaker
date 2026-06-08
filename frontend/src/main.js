244c8bb0583ee24968a5ea719b5fc9b411b3a2ff
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-08 10:08:33 +0800

    优化样式

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,5 @@
 import {createApp} from 'vue';
 import App from './Home.vue';
-import './style.css';
-import 'element-plus/dist/index.css';
-import 'element-plus/theme-chalk/dark/css-vars.css';
+import './style.scss';
 
 createApp(App).mount('#app');
