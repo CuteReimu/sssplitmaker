f0de9c25dd37b3d88adfd150cb5dbf92ba376104
奇葩の灵梦 <cutereimu@vip.qq.com>
2026-06-27 18:13:48 +0800

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri 'https://github.com/AlexKnauth/silksong-autosplit-wasm/releases/latest/download/silksong_autosplit_wasm_stable.wasm' -OutFile 'silksong_autosplit_wasm_stable.wasm' -v
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/LiveSplit/LiveSplit.AutoSplitters/master/LiveSplit.AutoSplitters.xml' -OutFile 'LiveSplit.AutoSplitters.xml' -v
-wails build -webview2 embed
+wails build
--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 curl -L -O https://github.com/AlexKnauth/silksong-autosplit-wasm/releases/latest/download/silksong_autosplit_wasm_stable.wasm
 curl -O https://raw.githubusercontent.com/LiveSplit/LiveSplit.AutoSplitters/master/LiveSplit.AutoSplitters.xml
 wails build
-wails build -platform=windows/amd64 -webview2 embed
+wails build -platform=windows/amd64
